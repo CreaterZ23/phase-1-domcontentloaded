54a3d2e1cbb76747b44356a5a119f12083efaffb
Zeon <zeonstewart23@gmail.com>
2021-08-16 12:27:23 -0400

    commit tree f4038ed81c27698ff8b15dfa7795f003d844e879

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    loadDom()
+});
+
+function loadDom(){
+    document.querySelector("#text").textContent = "This is really cool!"
+}
